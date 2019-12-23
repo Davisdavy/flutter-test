91f8128f855864e051b789190c7f089a417bdaaf
Davisdavy <davisdavy96@gmail.com>
2019-12-23 23:24:57 +0300

    Hot reload configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,41 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-    title: Text('First App'),
-      centerTitle: true,
-      backgroundColor: Colors.red[600],
-    ),
-    body: Center(
-      child: Text(
+void main() =>
+    runApp(MaterialApp(
+        home: Home(),
+    ));
+
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First App'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        child: Text(
           'Hello Ninjas',
-        style: TextStyle(
-          fontFamily: 'Montserrat',
-          fontSize: 20.0,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 2.0,
-          color: Colors.grey[400],
+          style: TextStyle(
+            fontFamily: 'Montserrat',
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[400],
+          ),
         ),
       ),
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: (){},
-      child: Text('click'),
-      backgroundColor: Colors.red[600],
-    ),
-  ),
-));
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('click us'),
+        backgroundColor: Colors.red[600],
+      ),
+    )
+    ;
+  }
+}
+
 
 
